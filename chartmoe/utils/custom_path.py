591852013865e1268e3da76a791fef7900560a8d
coobiw <bowenqu@stu.pku.edu.cn>
2024-08-30 00:58:05 +0800

    support custom path

---
+++ chartmoe/utils/custom_path.py
@@ -0,0 +1,6 @@
+# Model
+ChartMoE_HF_PATH = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/ckpt/chartmoe/chartmoe_hf'
+
+# Data
+ChartQA_ROOT = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/data/chart/ChartQA/'
+ChartQA_TEST_IMG_ROOT = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/data/chart/ChartQA/test/png/'
