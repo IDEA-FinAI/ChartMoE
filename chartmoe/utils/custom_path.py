5df8b06a3321404350dbcc8181dbd732470796fe
coobiw <bowenqu@stu.pku.edu.cn>
2024-09-04 16:34:54 +0800

    modify the hf_path to

--- chartmoe/utils/custom_path.py
+++ chartmoe/utils/custom_path.py
@@ -1,5 +1,5 @@
 # Model
-ChartMoE_HF_PATH = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/ckpt/chartmoe/chartmoe_hf'
+ChartMoE_HF_PATH = 'IDEA-FinAI/chartmoe'
 
 # Data
 ChartQA_ROOT = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/data/chart/ChartQA/'
