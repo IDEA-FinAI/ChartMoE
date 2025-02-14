1af375b772cd07295b2e20d66fb6d5564bcdb22f
Brian Qu <brian.bw.qu@gmail.com>
2025-02-14 20:09:07 +0800

    minor changes

--- chartmoe/utils/custom_path.py
+++ chartmoe/utils/custom_path.py
@@ -7,5 +7,5 @@
 ChartMoE_HF_PATH = 'IDEA-FinAI/chartmoe'
 
 # Data
-ChartQA_ROOT = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/data/chart/ChartQA/'
-ChartQA_TEST_IMG_ROOT = '/data/FinAi_Mapping_Knowledge/qiyiyan/qbw/cache/data/chart/ChartQA/test/png/'
+ChartQA_ROOT = '/path/to/ChartQA/'
+ChartQA_TEST_IMG_ROOT = '/path/to/ChartQA/test/png/'
